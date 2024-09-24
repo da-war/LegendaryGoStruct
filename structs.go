71ef518e5b10b51bd04aafb3ea696ae395e550f3
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-09-25 03:11:38 +0500

    struct initialized named user

--- structs.go
+++ structs.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello GoWorld")
+	firstName := getUserData("Please Enter your first name:")
+	lastName := getUserData("Please Enter your last name:")
+	birthDate := getUserData("Please Enter your Date of birth")
+
+	fmt.Print(firstName, lastName, birthDate)
+}
+
+func outPutUserDetails() {
+	fmt.Println("")
+}
+
+func getUserData(str string) string {
+	fmt.Println(str)
+	var value string
+	fmt.Scan(&value)
+	return value
 }
